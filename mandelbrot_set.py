bb0fd52a98e59c769699ac71e791f7c4e8c7de04
pc2 <yehan278@gmail.com>
2023-10-09 15:05:08 -0400

    .

--- mandelbrot_set.py
+++ mandelbrot_set.py
@@ -1,32 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
-N = 100
+from PIL import Image,ImageDraw
+
+Max_Iterations = 60
+width = 100
+height = 100
 
 def is_bound(c:complex):
-    z = 0j
-    for i in range(N):
-        z=z*z + c
-    if abs(z) > 2:
-        return False
-    else:
-        return True
 
-def Manset(N):
-    a = np.array((N,N))
+    z = 0j
+    for i in range(Max_Iterations):
+        z=z*z + c
+        if abs(z) > 2:
+            return False
+    return True
+
+def manSet():
+    a = np.zeros((width,height))
     i=0
-    while i < N:
+    while i < width:
         j=0
-        while j<N:
-            if is_bound(complex(i,j)):
+        while j<height:
+
+            if is_bound(complex(i,j)) == True:
                 a[i,j] = 1
+                j+=1
+                
             else:
                 a[i,j] = 0
+                j+=1
+        i+=1
 
     return a
 
+def graph(set):
+    fig,ax = plt.subplots(nrows=1,ncols=1)
+    for x in range(width):
+        for y in range(height):
+            if set[x,y] == 1:
+                ax.plot(x,y,marker='o')
 
 
 if __name__ == '__main__':
 
-    print(Manset(100))
-
+    a=manSet()
+    print(a)
+    # graph(a)
+    # plt.show()
