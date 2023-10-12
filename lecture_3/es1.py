a3df99c44bf75cbb4e62e3d4a8865d4ebf8dec7e
pc2 <yehan278@gmail.com>
2023-10-12 17:45:17 -0400

    es1 lec3

--- lecture_3/es1.py
+++ lecture_3/es1.py
@@ -1,16 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import ceil,sqrt
 import sys
 
+f   = str('eventi_gauss.txt')
 
 
 def file():
     '''opens the input file and outputs a list with the values as float'''
     sample = []
-    with open(sys.argv[1],'r') as input_file:
+    with open(f,'r') as input_file:
         for i in input_file:
-            sample.append(i)
+            sample.append(float(i))
     return sample
+
+def xMin(sample):
+
+    min=sample[0]
+    for i in sample:
+        if i<min:
+            min = i
+    return min
+
+
+def xMax(sample):
+    
+    max=sample[0]
+    for i in sample:
+        if i>max:
+            max = i
+    return max
 
 def positive(sample):
     '''prints the first 10 positive elements'''
@@ -19,29 +38,54 @@ def positive(sample):
     for i in sample:
         if i>=0:
             pos.append(i)
-    for j in range(10):
-        print(pos[j])
+    return pos
 
 def count(sample):
     '''returns the number of elements in the file'''
     return len(sample)
 
-def hist_N(sample,N:int):
-    tmp = []
-    for i in range(N):
-        tmp.append(i)
+def mean(sample):
+    '''first momentum'''
+    return (sum(sample)/len(samples))
 
-    hist(tmp)
+def variance(sample):
+    s = 0
+    m = mean(sample)
+    for i in sample:
+        s += ((i-m)*(i-m))
+    return s/len(sample)
+
+def stdDeviation(sample):
+    return sqrt(variance(sample))
+
+def sturges(sample):
+
+    N = len(sample)
+    return ceil(1+3.322*np.log(N))
 
 def hist(sample):
 
+    N_bins = sturges(sample)
+    bin_edges = np.linspace(xMin(sample),xMax(sample),N_bins)
+
     fix,ax = plt.subplots(nrows= 1, ncols=1)
-    ax.hist(sample,color = 'orange')
+    ax.hist(sample,color = 'salmon',bins=bin_edges)
     plt.show()
+
+def hist_N(sample,N:int):
+
+    tmp = []
+    for i in range(N):
+        tmp.append(sample[i])
+    hist(tmp)
+
 
 
 
 if __name__ == '__main__':
+
+    # N = int(input("Number of parameters:\t"))
     samples = file()
-    # positive(samples)
+    print(variance(samples))
     hist(samples)
+    # hist_N(samples,N)
