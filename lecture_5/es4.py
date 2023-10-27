d306b83d55f0ba1da503102a111f86e29ac8be3b
Yedi278 <yehan278@gmail.com>
2023-10-27 23:03:34 +0200

    es4 finished

--- lecture_5/es4.py
+++ lecture_5/es4.py
@@ -1,13 +1,36 @@
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import numpy as np
 
 def file_open(name):
     
-    with open(name,'r') as input:
-        list = [x for x in input]
+    with open(name,'r',) as input:
+        list = [float(x.strip('\n')) for x in input]
         return list
-    
+
+
+
+
 
 if __name__ == '__main__':
 
-    file = 'eventi_gauss.txt'
+    file = 'eventi_unif.txt'
+    data = file_open(file)
+    
+    mean = np.mean(data)
 
-    print(file_open(file))
+    larger = list(filter(lambda x: x > mean,data))
+    smaller = list(filter(lambda x: x < mean,data))
+
+    fig,ax = plt.subplots(1,2)
+
+    ax[0].hist(larger)
+    ax[1].hist(smaller)
+
+    print(np.std(data))
+    print(np.std(larger))
+    print(np.std(smaller))
+
+    plt.show()
+
+    
