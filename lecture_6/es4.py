cec72aebff80b5ae191853f1b5cd7524dacabbff
Yedi278 <yehan278@gmail.com>
2023-11-05 22:04:44 +0100

    finished

--- lecture_6/es4.py
+++ lecture_6/es4.py
@@ -1,27 +1,46 @@
+from math import sqrt
+
 '''Determine the minimum of the function g(x) = x2 + 7.3x + 4 
     using the golden ratio search method in the interval (-10, 10).'''
 
-#   x_zero = -3.65
+r = (-1+sqrt(5))/2
 
-from math import sqrt
+def f(x):
+
+    return -((x-1)**2)
 
 def g(x):
-    return x*x + 7.3*x +4
+    return x**2 + (7.3*x) +4
 
 def minimum(func,xmin,xmax,precision):
-
-    r = (-1+sqrt(5))
     
-    if(sqrt((xmax-xmin)*(xmax-xmin)) < precision):
+    if(sqrt((xmax-xmin)**2) < precision):
         return xmin
     
     x2 = xmin +     r*(xmax-xmin)
     x3 = xmin + (1-r)*(xmax-xmin)
     
-    if(func(x3) < func(x2)):
-        return minimum(func,xmin,x3,precision)
+    if(func(x3) > func(x2)):
+        return minimum(func,x3,xmax,precision)
     else:
-        return minimum(func,x2,xmax,precision)
-    
+        return minimum(func,xmin,x2,precision)
 
-print(minimum(g,-10,10,0.01))
+def maximum(func,xmin,xmax,precision):
+    
+    d = sqrt((xmax-xmin)**2)
+
+    if(d < precision):
+        return xmin
+    
+    x2 = xmin +     r*d
+    x3 = xmin + (1-r)*d
+    
+    if(func(x3) < func(x2)):
+        return maximum(func,x3,xmax,precision)
+    else:
+        return maximum(func,xmin,x2,precision)
+
+
+print("Minimum of g is:",minimum(g,-10,0,0.000001))
+
+print("maximum of f is:",maximum(f,-3,3,0.0000001))
