fa0ab33b2140452034d2785b61307346f2719d1b
Yedi278 <yehan278@gmail.com>
2023-11-06 23:03:02 +0100

    return error

--- lecture_6/es8.py
+++ lecture_6/es8.py
@@ -27,8 +27,8 @@ def graph(f,x1,x2,y1,y2):
     fig = plt.figure()
     ax = fig.add_subplot(111,projection='3d')
 
-    x = np.arange(x1,x2,0.05)
-    y = np.arange(y1,y2,0.05)
+    x = np.arange(x1,x2,0.005)
+    y = np.arange(y1,y2,0.005)
     
     X,Y = np.meshgrid(x,y)
     Z = f(X,Y)
@@ -36,39 +36,47 @@ def graph(f,x1,x2,y1,y2):
 
     ax.plot_surface(X,Y,Z)
     # ax.plot_surface(X,Y,Z2)
+
+    return ax
+
+def min(f,x1,x2,y1,y2,m=None,prec = 0.001):
     
+    r = (sqrt(5)-1)/2
+    r = r*(x2-x1)
+
+    if m == None:
+        m = ((x1+x2)/2,(y1+y2)/2)
+    
+    p1 = (m[0]+r,m[1])
+    p2 = (m[0]-r,m[1])
+    p3 = (m[0],m[1]+r)
+    p4 = (m[0],m[1]-r)
+
+    p = {p1,p2,p3,p4}
+    min = (p1[0],p1[1])
+
+    if sqrt(f(m[0],m[1])-f(min[0],min[1]))**2 < prec:
+        return m[0],m[1]
+    
+    
+    for i in p:
+
+        if f(i[0],i[1]) < f(min[0],min[1]):
+            min = i[0],i[1]
+    a = 1
+    b = 1
+    c = 1
+    d = 1
+
+    return min(f,a,b,c,m=(2,3))
+
+    #calcolo distanza tra punto minimo e punto medio
+    #se sotto precisione ho trovato il valore
+    #altrimenti reitero togliendo un intervallo
+
+
+if __name__ == '__main__':
+
+    ax = graph(fun_1,-6,6,-6,6)
+    print(min(fun_1,-6,6,-6,6))
     plt.show()
-
-def min(f,x1,x2,y1,y2,prec = 0.001  ):
-    
-    r = 4
-    
-    xm = (x1+x2)/2
-    ym = (y1+y2)/2
-
-    while f(xm,ym) - f(xm+r,xm-r) > prec:
-    
-        p = np.empty((2,4))
-        p[0] = (xm-r,xm+r)
-        p[1] = (ym-r,ym+r)
-
-        a = f(p[0,0])
-        
-        for i,j in p:
-            if f(p[i,j]) < a:
-                a = f(p[i,j])
-
-
-        xm,ym = a[0],a[1]
-
-
-
-    return xm,ym
-
-
-
-
-
-graph(torus,-6,6,-6,6)
-
-    
