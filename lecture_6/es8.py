1e7ac7306860ca73a8e06160ab5c0ca1efa2490e
pc2 <yehan278@gmail.com>
2023-11-08 07:34:48 -0500

    adjusted noise

--- lecture_6/es8.py
+++ lecture_6/es8.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt,pi,cos,sin
-from functions import f,f1,f2
+import functions as f
 import random as rnd
 
 def graph(f,x1,x2,y1,y2):
@@ -18,7 +18,7 @@ def graph(f,x1,x2,y1,y2):
     X,Y = np.meshgrid(x,y)
     Z = f(X,Y)
 
-    ax.plot_surface(X,Y,Z)
+    ax.plot_surface(X,Y,Z, alpha=.2)
 
     return ax
 
@@ -34,8 +34,9 @@ def points(m,k,r):
 
 def min(f,x1,x2,y1,y2,prec = 0.001):
     
-    
-    r = 1/2*np.sqrt((x2-x1)**2)
+    buffer = []
+
+    r = .5*abs(x2-x1)
 
 
     m = ((x1+x2)/2,(y1+y2)/2)
@@ -43,38 +44,44 @@ def min(f,x1,x2,y1,y2,prec = 0.001):
 
     while r > prec:
 
+
         p = points(m,10,r)
                 
         for i in p:
 
+            #il valore deve restare nell'intervallo del dominio e deve essere più piccolo
             if f(i[0],i[1]) < f(min[0],min[1]) and i[0]<x2 and i[0]>x1 and i[1]<y2 and i[1]>y1:
-                min = i[0],i[1]
+                min = i
+        
+        buffer.append(min)
         
         m = min
-        r = (1/2 + rnd.random())*r
+        r *= (0.35+rnd.random())
 
-    return min
+    return min,buffer
 
 
 if __name__ == '__main__':
 
     center = (0,0)
-    r = 2
+    r = 4
 
     x1 = center[0] - r
     x2 = center[0] + r
     y1 = center[1] - r
     y2 = center[1] + r
 
-    func = f1
+    func = f.f2
 
     ax = graph(func,x1,x2,y1,y2)
 
     
-    m = min(func,x1,x2,y1,y2)
+    m,buffer = min(func,x1,x2,y1,y2)
+
 
     ax.scatter(m[0],m[1],func(m[0],m[1]), color='red', marker = '*')
     
-    # ax.scatter(0,0,func(0,0), color='green', marker = '*')
+    for i in buffer:
+        ax.scatter(i[0],i[1],func(i[0],i[1]),color='green',marker='*')
 
     plt.show()
--- lecture_6/functions.py
+++ lecture_6/functions.py
@@ -1,27 +1,18 @@
 import numpy as np
 
-def fun_1(x, y):
-    return x**2 / np.sqrt(x**2 + y**2)
-
-def fun_2(x, y):
-    return x / np.sqrt(x**2 + y**2)
-
-def fun_3(x, y):
-    return x**2 * y / (x**4 + y**2)
-
-def fun_4(x, y):
-    return x * y / (x**2 + y**2)
-
 def torus(x,y):
     r = 1
     R = 4
-    return np.sqrt(r**2-(R-np.sqrt((x**2)+(y**2)))**2)
+    return -np.sqrt(r**2-(R-np.sqrt((x**2)+(y**2)))**2)
 
 def f(x,y):
     return x*(x-2)*((x**2)-7)
 
 def f1(x,y):
-    return -np.power(2.71,-((x)**4+y**4))+pow(2.71,-x**2-y**2)
+    return -np.power(2.71,-((x+1)**4+y**4))+pow(2.71,-x**2-y**2)
 
 def f2(x,y):
-    return -pow(2.71,-x**2-y**2)
+    return .3*np.sin((x+1)*y)-np.power(2.71,-(x**2+y**2))
+
+def f3(x,y):
+    return -2.71**(-(x+1)**2-y**2)
