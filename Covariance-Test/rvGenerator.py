0b2e59c11a689bd35d3f4d5ee23218b60d147f5e
Yehan Edirisinghe <yedi278@gmail.com>
2023-11-24 15:44:59 +0100

    .

--- Covariance-Test/rvGenerator.py
+++ Covariance-Test/rvGenerator.py
@@ -9,46 +9,57 @@ import matplotlib.pyplot as plt
 
 def noCorrelation(N):
 
-    x = np.random.uniform(0,1,N)
-    y = np.random.uniform(0,1,N)
+    x = np.random.uniform(0,np.pi,N)
+    # y = np.random.uniform(0,1,N)
 
-    # y = np.array([np.sin(i*(5+np.random.uniform(0,.25))) for i in x])
+    y = np.array([np.sin(i*(5+np.random.uniform(0,.25))) for i in x])
 
     return x,y
 
 def linearCorrelation(N):
 
     x = np.random.normal(size=N)
+
     y = np.array([(i*(5+np.random.uniform(0,.35))) for i in x])
 
     return x,y
 
-def covariance(x,y):
+def cov(x,y):
     
-    v = x,y
+    sum = 0
+    m = np.average(x)
+    n = np.average(y)
 
-    cov = np.empty((2,2))
+    for i in range(len(x)):
+        
+        sum += (x[i]-m)*(y[i]-n)
 
-    for l in range(2):
-        for k in range(2):
-            
-            cov[l,k] = stdDeviation(v[l])*stdDeviation(v[k])
+    return sum/len(x)
 
-    r = (cov[0,1]*cov[1,0])/(cov[1,1]*cov[0,0])
+def covMatrix(x,y):
 
-    return cov,r
+    mat = np.empty((2,2))
+
+    mat[0,0] = cov(x,x)
+    mat[0,1] = cov(x,y)
+    mat[1,0] = cov(y,x)
+    mat[1,1] = cov(y,y)
+
+    r = mat[1,0]*mat[0,1]/(mat[0,0]*mat[1,1])
+
+    return mat,r**2
 
 if __name__ == '__main__':
 
     fig,ax = plt.subplots(1,1)
 
-    x,y = noCorrelation(2000)
-    # x,y = linearCorrelation(2000)
+    # x,y = noCorrelation(2000)
+    x,y = linearCorrelation(2000)
 
     ax.scatter(x,y)
 
-    cov,r = covariance(x,y)
+    cov,r = covMatrix(x,y)
 
-    print(cov,'\n',r)
+    print(cov,'\n\n','Correlation:',r)
 
     plt.show()
